2dd1127932957b1e44d7a8b989bb9b635e34a254
Robby F <robby@grove-financial.com>
2022-03-12 16:36:57 -0500

    Update options_data.py

--- examples/options_data.py
+++ examples/options_data.py
@@ -1,11 +1,14 @@
+import PyTradier
+
+tradier = Tradier(token='sGb6eUDEJc5DS9KYB6LYTCxGACWO', account_id='6YA18757', endpoint='brokerage')
+
 import Terminate_prog as tp  # to use: tp.Terminate (<msg>)
-
-from pytradier.tradier import Tradier
-
 tp.Terminate ("Debug STOP!")
 
+#from pytradier.tradier import Tradier
+
+
 # authenticate with the Tradier API
-tradier = Tradier(token='sGb6eUDEJc5DS9KYB6LYTCxGACWO', account_id='6YA18757', endpoint='brokerage')
 #tradier = Tradier(token='abc123', account_id='123456', sandbox=False)
 
 
